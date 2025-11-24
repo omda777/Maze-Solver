394acf28d5c422d1bfaada32f8ac2803e805f5ec
Abdelrahman Emad <abdoemad0120@gmail.com>
2025-11-25 00:36:49 +0200

    change color of path

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -3,5 +3,5 @@ export const startColor ='#22c55e';
 export const endColor = '#ef4444' ;
 export const wallColor = 'black';
 export const emptyCellColor = 'white';
-export const pathColor ='#26f801ff';
-export const visulColor = '#00bbfffc';
+export const pathColor ='DodgerBlue';
+export const visulColor = 'gold';
